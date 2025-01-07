d832ec6dcf27b4bd784b696e299bf763ae3a7e9e
mulonemartin <mulone.martin@gmail.com>
2025-01-07 10:01:29 -0300

    fix: added flatten contracts

--- scripts/contract_flattener.sh
+++ scripts/contract_flattener.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 echo "Starting to flatten our contracts"
 node_modules/.bin/truffle-flattener contracts/MocMedianizer.sol > scripts/contract_flatten/MocMedianizer_flat.sol
+node_modules/.bin/truffle-flattener contracts/price-feed/feed-factory.sol > scripts/contract_flatten/feed-factory_flat.sol
+node_modules/.bin/truffle-flattener contracts/price-feed/price-feed.sol > scripts/contract_flatten/price-feed_flat.sol
+node_modules/.bin/truffle-flattener contracts/authority/MoCGovernedAuthority.sol > scripts/contract_flatten/MoCGovernedAuthority_flat.sol
 echo "Finish successfully! Take a look in folder scripts/contract_flatten/..."
